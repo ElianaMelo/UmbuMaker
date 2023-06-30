ea85d6fa93e52072ee8394bd366664f0229698b5
tamiresxavier <tx977459@gmail.com>
2023-06-30 20:02:19 -0300

    Alteração e funcionamento do CRUD

---
+++ src/main/java/br/edu/ifpb/umbumaker/UmbuMakerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.umbumaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbuMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbuMakerApplication.class, args);
+	}
+
+}
